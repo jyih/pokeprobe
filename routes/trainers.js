1b6dc57d3990d473f02260534a98930acf75997f
Brandon <omstachu626@gmail.com>
2021-06-28 14:44:11 -0700

    working on form validation

--- routes/trainers.js
+++ routes/trainers.js
@@ -1,23 +1,53 @@
 var express = require('express');
 var router = express.Router();
-const { csrfProtection, asyncHandler } = require('./utils')
+const { csrfProtection, asyncHandler, handleValidationErrors } = require('./utils')
 const db = require('../db/models')
 const { Trainer } = db
 const bcrypt = require('bcrypt')
+const { check, validationResult } = require("express-validator")
     // express validator necessary
 
 /* GET users listing. */
+const createFalsyCheck = (fieldName, messageName, message = `${messageName} cannot be blank`) => {
+    return check(fieldName)
+        .exists({ checkFalsy: true })
+        .withMessage(message)
+}
+
+
+
+const formValidation = [
+    check('trainerName')
+    .exists({ checkFalsy: true })
+    .withMessage('Trainer Name cannot be blank'),
+    check('firstName')
+    .exists({ checkFalsy: true })
+    .withMessage('First Name cannot be blank'),
+    check('lastName')
+    .exists({ checkFalsy: true })
+    .withMessage('Last Name cannot be blank'),
+    check('email')
+    .exists({ checkFalsy: true })
+    .withMessage('Email cannot be blank'),
+    check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Password cannot be blank'),
+    check('confirmPassword')
+    .exists({ checkFalsy: true })
+    .withMessage('Confirm Password cannot be blank'),
+]
+
 // ? change route to something like /signup
 router.get('/', csrfProtection, asyncHandler(async(req, res) => {
     res.render('trainer-signup', { csrfToken: req.csrfToken() })
 }));
 
-router.post('/', csrfProtection, asyncHandler(async(req, res) => {
+router.post('/', formValidation, csrfProtection, asyncHandler(async(req, res) => {
     console.log(req.body)
-    const { username, firstName, lastName, email, bio, password } = req.body
+    const { trainerName, firstName, lastName, email, bio, password } = req.body
     const hashedPassword = await bcrypt.hash(password, 10)
     await Trainer.create({
-        username,
+        username: trainerName,
         firstName,
         lastName,
         email,
