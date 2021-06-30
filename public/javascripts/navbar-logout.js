3fef683e4a085c4f5ba8648d324fa0b3c615c7fe
Levi-Dominguez <josue.dominguez2@g.austincc.edu>
2021-06-30 13:41:27 -0700

    minor work

--- public/javascripts/navbar-logout.js
+++ public/javascripts/navbar-logout.js
@@ -1,9 +1,7 @@
 window.addEventListener("DOMContentLoaded", event => {
-    let { logoutTrainer } = require("../../auth");
-    console.log(logoutTrainer)
+    
     const logout = document.getElementById("logout-button")
     logout.addEventListener("click", event => {
-        logoutTrainer(req, res)
         console.log("hi")
     })
 })
