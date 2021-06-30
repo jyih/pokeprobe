4c9b222f9da7636ec8c1f215e04e0e05d408c352
Brandon <omstachu626@gmail.com>
2021-06-30 14:34:57 -0700

    got header to exist, and made logout button work

--- public/javascripts/navbar-logout.js
+++ public/javascripts/navbar-logout.js
@@ -1,9 +1,6 @@
 window.addEventListener("DOMContentLoaded", event => {
-    let { logoutTrainer } = require("../../auth");
-    console.log(logoutTrainer)
     const logout = document.getElementById("logout-button")
     logout.addEventListener("click", event => {
-        logoutTrainer(req, res)
-        console.log("hi")
+        window.location.assign("/trainers/logout")
     })
 })
