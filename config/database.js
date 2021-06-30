cff5755f9c5b870174509d1fbb4f6d75c960526e
jojo <johnathanyih@gmail.com>
2021-06-30 09:01:01 -0700

    database.js changes

--- config/database.js
+++ config/database.js
@@ -1,5 +1,7 @@
 const {
-    db: { username, password, database, host },
+    db: { username, password, database, host,
+        // use_env_variable 
+    },
 } = require('./index');
 
 module.exports = {
@@ -9,12 +11,16 @@ module.exports = {
         database,
         host,
         dialect: 'postgres',
-        seederStorage: 'sequelize',
-        logging: false,
     },
     production: {
-        use_env_variable: 'postgres://fsuvjbmmaeubjo:b9aab111b2c94e9b07e1d9a818a0ebfe7e5f3d6bc542b639c11156a9826ae69a@ec2-3-214-136-47.compute-1.amazonaws.com:5432/dengsrt0qnteod',
+        use_env_variable: 'DATABASE_URL',
         dialect: 'postgres',
         seederStorage: 'sequelize',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        }
     }
 };
