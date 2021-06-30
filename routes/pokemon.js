a7880d702a3c4477eb0724c7bbf56a3e9c62f3e6
michellekontoff <mlkontoff@gmail.com>
2021-06-30 16:30:58 -0400

    finished post for pokemon and pokepage creation on /fusion-pokemon

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -6,7 +6,7 @@ const {
   handleValidationErrors,
 } = require("./utils");
 const db = require("../db/models");
-const { Trainer, Type, Pokedex, FusionPokemon } = db;
+const { Trainer, Type, Pokedex, FusionPokemon, PokePage } = db;
 const { check, validationResult } = require("express-validator");
 
 router.get('/', csrfProtection, (req, res) => {
@@ -15,8 +15,25 @@ router.get('/', csrfProtection, (req, res) => {
   })
 })
 
-// router.post('/', {
+router.post('/', csrfProtection, asyncHandler(async (req, res) => {
+    const { nickname, pokedexId1, pokedexId2, description } = req.body;
+    const trainerId = req.session.auth.userId
 
-// })
+    const pokemon = await FusionPokemon.create({
+        nickname,
+        description,
+        pokedexId1,
+        pokedexId2,
+        trainerId
+    })
+
+    const pokepage = await PokePage.create({
+        content: description,
+        trainerId,
+        fusionPokemonId: pokemon.id,
+    })
+
+    res.redirect(`/pokepages/${pokepage.id}`);
+}))
 
 module.exports = router;
