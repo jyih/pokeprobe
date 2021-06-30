e0b77e19278eedcc0edc31291c71a7e6b271e2aa
jojo <johnathanyih@gmail.com>
2021-06-30 08:24:34 -0700

    first push to heroku

--- config/database.js
+++ config/database.js
@@ -12,4 +12,9 @@ module.exports = {
         seederStorage: 'sequelize',
         logging: false,
     },
+    production: {
+        use_env_variable: 'postgres://fsuvjbmmaeubjo:b9aab111b2c94e9b07e1d9a818a0ebfe7e5f3d6bc542b639c11156a9826ae69a@ec2-3-214-136-47.compute-1.amazonaws.com:5432/dengsrt0qnteod',
+        dialect: 'postgres',
+        seederStorage: 'sequelize',
+    }
 };
