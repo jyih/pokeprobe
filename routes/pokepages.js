574a9a92f6009afdb9af9b2fac07176146a1674e
michellekontoff <mlkontoff@gmail.com>
2021-07-01 11:44:42 -0400

    can properly reference types all the way from pokepage

--- routes/pokepages.js
+++ routes/pokepages.js
@@ -18,6 +18,27 @@ router.get('/', asyncHandler(async(req, res) => {
     res.render("pokepages/pokepages", { pokePages })
 }))
 
+
+router.get('/test', asyncHandler(async (req, res) => {
+    const pokedex = await Pokedex.findByPk(2, {
+        include: [
+            {
+            model: Type,
+            as: 'Type1'
+            },
+            {
+            model: Type,
+            as: 'Type2'
+            }
+        ]
+    });
+
+    // const type = await Type.findByPk(1)
+
+    console.log(pokedex.Type1)
+
+}))
+
 router.get('/:id', asyncHandler(async(req, res) => {
     const pokePage = await findFusion(req.params.id)
     const fusionInfo = await findFusionInfo(req.params.id)
@@ -30,6 +51,7 @@ router.get('/:id', asyncHandler(async(req, res) => {
     const imgUrl = `https://images.alexonsager.net/pokemon/fused/${id1}/${id1}.${id2}.png`
     res.render("pokepages/pokepages-id", { imgUrl, nickname, description })
 }))
+
 
 
 
