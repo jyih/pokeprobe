f56e7103b8ea45e69b0053285267197cb632325f
michellekontoff <mlkontoff@gmail.com>
2021-06-30 22:55:29 -0400

    finished seeders for trainers, pokemon, and pokepages

---
+++ db/seeders/20210701022105-add-dummy-pokepages.js
@@ -0,0 +1,66 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+      return queryInterface.bulkInsert('PokePages', [
+          {
+              content: 'He pinched me once while I was building a sandcastle. It really hurt.',
+              trainerId: 2,
+              fusionPokemonId: 1,
+              createdAt: new Date(),
+              updatedAt: new Date()
+          },
+          {
+              content: 'I just love her little horn, don\'t you?',
+              trainerId: 2,
+              fusionPokemonId: 2,
+              createdAt: new Date(),
+              updatedAt: new Date()
+          },
+          {
+              content: `He's never gonna give you. Never gonna let you down.`,
+              trainerId: 2,
+              fusionPokemonId: 3,
+              createdAt: new Date(),
+              updatedAt: new Date()
+          },
+          {
+              content: '',
+              trainerId: 3,
+              fusionPokemonId: 4,
+              createdAt: new Date(),
+              updatedAt: new Date()
+          },
+          {
+              content: 'I took a bath with him once. Don\'t recommend it.',
+              trainerId: 4,
+              fusionPokemonId: 5,
+              createdAt: new Date(),
+              updatedAt: new Date()
+          },
+          {
+              content: 'All bow to our one and only king.',
+              trainerId: 4,
+              fusionPokemonId: 6,
+              createdAt: new Date(),
+              updatedAt: new Date()
+          }
+      ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+      return queryInterface.bulkDelete('PokePages', null, {});
+  }
+};
