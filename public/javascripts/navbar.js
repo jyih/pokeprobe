85e99d22ad7e6dd0d16137182420c097e8f3c601
Ji <jikyung@gmail.com>
2021-07-01 13:06:56 -0400

    fixed nav catch,login,logout

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -1,9 +1,8 @@
 
-window.addEventListener("DOMContentLoaded", event => {
-    const logout = document.getElementById("logout-button")
-    logout.addEventListener("click", event => {
-        console.log('hi')
-        console.log(event.target.id)
-       window.location.assign("/trainers/logout")
-    })
-})
+// window.addEventListener("DOMContentLoaded", event => {
+//     const logout = document.getElementById("logout-button")
+//     logout.addEventListener("click", event => {
+
+//         window.location.assign("/trainers/logout")
+//     })
+// })
