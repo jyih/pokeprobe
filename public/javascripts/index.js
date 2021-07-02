6dd3a3d04c276e5e1a5f8be24b8ad1b3fb31c492
jojo <johnathanyih@gmail.com>
2021-07-01 19:46:41 -0700

    tied loose ends

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,11 @@
-window.addEventListener("load", (event)=>{
+window.addEventListener("DOMContentLoaded", (event) => {
     console.log("hello from javascript!")
+    const searchButton = document.querySelector('.mag-icon');
+    const memes = document.querySelector('.memes')
+    const searchBar = document.querySelector('.search-bar-spot')
+    searchButton.addEventListener('click', event => {
+        memes.innerHTML = 'pOkeMAns'
+        searchBar.placeholder = 'Feature to be implemented...'
+        if (searchBar.value) searchBar.value = 'Feature to be implemented...'
+    })
 })
