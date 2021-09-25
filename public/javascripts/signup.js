f4367f1a22cc7a2922036a06e40b4825a1a6a49b
michellekontoff <mlkontoff@gmail.com>
2021-09-25 18:48:24 -0400

    fixed error to check both keyup on inputs

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -2,12 +2,20 @@ window.addEventListener("DOMContentLoaded", (event) => {
     const passwordInput = document.querySelector('.password-input')
     const confirmPasswordInput = document.querySelector('.confirm-password-input')
     const passwordMatchCheck = document.getElementById('password-match-check')
-    
-    confirmPasswordInput.addEventListener('keyup', e => {
+
+    function checkPasswords() {
         if (passwordInput.value !== confirmPasswordInput.value) {
             passwordMatchCheck.innerHTML = 'Passwords don\'t match.'
         } else {
             passwordMatchCheck.innerHTML =''
         }
+    }
+    
+    confirmPasswordInput.addEventListener('keyup', e => {
+        checkPasswords()
+    })
+
+    passwordInput.addEventListener('keyup', e => {
+        checkPasswords()
     })
 })
