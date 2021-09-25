bde27631b32af39a46991f458d988ff1ef9dd22f
michellekontoff <mlkontoff@gmail.com>
2021-09-25 15:20:01 -0400

    made pug template and attached stylesheet

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -2,6 +2,12 @@ window.addEventListener("DOMContentLoaded", (event) => {
     document.getElementById('search-button').addEventListener('click', e => {
         const searchField = document.getElementById('search-bar');
 
+        if (!searchField.value) {
+            searchField.value = 'Please entire a search term.'
+
+            return
+        }
+
         window.location.href = `/search/${searchField.value}`
     })
 })
