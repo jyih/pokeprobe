e9ad56134c7ff5c5a9996f809f9fc95dc5a6948f
michellekontoff <mlkontoff@gmail.com>
2021-09-25 18:43:39 -0400

    removed unused code

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -4,10 +4,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
     const passwordMatchCheck = document.getElementById('password-match-check')
     
     confirmPasswordInput.addEventListener('keyup', e => {
-        // const passwordInput = document.getElementById('password-input')
-        // const confirmPasswordInput = document.getElementById('confirm-password-input')
-        // const passwordMatchCheck = document.getElementById('password-match-check')
-        console.log('h')
         if (passwordInput.value !== confirmPasswordInput.value) {
             passwordMatchCheck.innerHTML = 'Passwords don\'t match.'
         } else {
